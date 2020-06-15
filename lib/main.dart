e0438f3806fca1dc469d7f6a69bf1e9a130fd241
JAPM <cicelcup@gmail.com>
2020-06-15 15:15:31 -0600

    Merge cards in one screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'my_card.dart';
-import 'my_first_card.dart';
 import 'my_second_card.dart';
+import 'my_first_card.dart';
+import 'my_card.dart';
 import 'theme.dart';
 
 void main() {
@@ -12,10 +12,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My Card',
-      theme: appTheme(),
-      home: MyCard(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "My Card",
+        theme: appTheme(),
+        home: Options());
+  }
+}
+
+class Options extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("My card example"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(
+            children: <Widget>[
+              RaisedButton(
+                color: Colors.green,
+                padding: EdgeInsets.all(16.0),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyFirstCard()),
+                  );
+                },
+                child: Text(
+                  "My Card 1",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              RaisedButton(
+                color: Colors.redAccent,
+                padding: EdgeInsets.all(16.0),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MySecondCard()),
+                  );
+                },
+                child: Text(
+                  "My Card 2",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              RaisedButton(
+                padding: EdgeInsets.all(16.0),
+                color: Colors.purpleAccent,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyCard()),
+                  );
+                },
+                child: Text(
+                  "My Card 3",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      .copyWith(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
